a1e4be504bd9f0852c5d090f0fb3181dc256a2d9
Fan1c <aleksey_304@bk.ru>
2025-06-05 19:42:32 +0400

    Update Laba 6.py

--- Laba 6.py
+++ Laba 6.py
@@ -33,16 +33,13 @@ def F_iter(n):
     fn_minus_2 = 10  # F(0)
     fn_minus_1 = 10  # F(1)
     factorial = 2 # (2n)!
-
+    fact_fn_minus_2=3628800
     for i in range(2, n + 1):
-        factorial *= (2 * i) * (2 * i - 1)  # Вычисление (2i)!
-        fact_fn_minus_2 = 1
-        for j in range(1, fn_minus_2 + 1):
-            fact_fn_minus_2 *= j
+        factorial *= (2 * i) * (2 * i - 1)
+        fact_fn_minus_2 *= fn_minus_2
         fn = (1 if i % 2 == 0 else -1) * (fact_fn_minus_2 - ((2 * fn_minus_1) / factorial if 2*i < 171 else float('inf')))
-        # Обновление значений для следующей итерации
+        #Обновляем значения для следующей итерации
         fn_minus_2, fn_minus_1 = fn_minus_1, fn
-
     return fn_minus_1
 
 
